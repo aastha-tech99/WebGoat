b5722e5107141eeb64901f5d4b9a0e97e822a6e7
Harness Auto-Remediation <remediation@harness.io>
2026-09-23 04:42:39 +0000

    vuln-fix: Remove PII from console log statements (STO-vefv7UrDrOgfeuPsbIeSNQ)
    Remove server response data and PII-suggestive labels from console.log to comply with data minimization principles.
    
    Weakness: CWE-532
    Severity: High
    Detection: Tool
    Signed-off-by: Harness Auto-Remediation <remediation@harness.io>
    Co-authored-by: Harness Auto-Remediation <remediation@harness.io>

--- src/main/resources/webgoat/static/js/goatApp/view/GoatRouter.js
+++ src/main/resources/webgoat/static/js/goatApp/view/GoatRouter.js
@@ -68,7 +68,7 @@ define(['jquery',
 
             // shim to support xss lesson
             webgoat.customjs.phoneHome = function (e) {
-                console.log('phoneHome invoked');
+                console.log('callback invoked');
                 webgoat.customjs.jquery.ajax({
                     method: "POST",
                     url: "CrossSiteScripting/phone-home-xss",
@@ -78,8 +78,7 @@ define(['jquery',
                     },
                     contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
                     success: function (data) {
-                        //devs leave stuff like this in all the time
-                        console.log('phone home said '  + JSON.stringify(data));
+                        // response data is not logged to avoid leaking PII
                     }
                 });
             }
